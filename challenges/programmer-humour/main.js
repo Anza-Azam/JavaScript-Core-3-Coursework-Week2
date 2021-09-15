8efe0a6ecc8a26baa800b4f12c3026d93c67132b
Anza-Azam <83141825+Anza-Azam@users.noreply.github.com>
2021-09-15 10:53:32 +0100

    challenges done

--- challenges/programmer-humour/main.js
+++ challenges/programmer-humour/main.js
@@ -1,21 +1,15 @@
-function randomFun() {
-    
+function randomFun () {
+  fetch('https://xkcd.now.sh/?comic=latest')
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
 
+      const renderImage = document.createElement('img')
+      document.body.prepend(renderImage)
+      renderImage.src = data.img
+      renderImage.alt = data.alt
+    })
 
-    fetch('https://xkcd.now.sh/?comic=latest').then(response => response.json()).then(data => {
-        console.log(data);
-    
-        const renderImage = document.createElement('img');
-        document.body.prepend(renderImage);
-        renderImage.src = data.img;
-        renderImage.alt = data.alt;
-
-    }
-    )
-        
-        
-        
-        
-        .catch(error => console.log(error));
+    .catch(error => console.log(error))
 }
-randomFun();
+randomFun()
